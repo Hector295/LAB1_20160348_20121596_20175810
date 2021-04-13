52aa9f432bb0b0283c434a65c7354a95120cc29d
Fabrizio <a20160348@pucp.edu.pe>
2021-04-12 20:27:07 -0500

    Controller y vista juegos

--- Lab1_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab1_gtics_20211/controller/JuegosController.java
+++ Lab1_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab1_gtics_20211/controller/JuegosController.java
@@ -1,17 +1,30 @@
 package edu.pucp.gtics.lab1_gtics_20211.controller;
 
+import edu.pucp.gtics.lab1_gtics_20211.repository.JuegosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/juegos")
 public class JuegosController {
 
-    public String listaJuegos (){
-;
+    @Autowired
+    JuegosRepository juegosRepository;
+
+    @GetMapping("/listaJuegos")
+    public String listaJuegos(Model model) {
+        model.addAttribute("listaJuegos", juegosRepository.findAll());
+        return "juegos/lista";
     }
 
-    public String editarJuegos(){
+    public String editarJuegos() {
 
     }
 
-    public String guardarJuegos(){
+    public String guardarJuegos() {
 
     }
 
