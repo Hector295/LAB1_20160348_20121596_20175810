092cb20b1ed4f0d025bbdfb924691a3a6c474702
sarita atanacio <atanacio.sarita@pucp.pe>
2021-04-12 21:02:35 -0500

    Juegos Editar

--- Lab1_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab1_gtics_20211/controller/JuegosController.java
+++ Lab1_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab1_gtics_20211/controller/JuegosController.java
@@ -2,12 +2,16 @@ package edu.pucp.gtics.lab1_gtics_20211.controller;
 
 import edu.pucp.gtics.lab1_gtics_20211.entity.Juegos;
 import edu.pucp.gtics.lab1_gtics_20211.repository.JuegosRepository;
+import edu.pucp.gtics.lab1_gtics_20211.repository.PlataformasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/juegos")
@@ -16,13 +20,27 @@ public class JuegosController {
     @Autowired
     JuegosRepository juegosRepository;
 
+    @Autowired
+    PlataformasRepository plataformasRepository;
+
     @GetMapping("/listaJuegos")
     public String listaJuegos(Model model) {
         model.addAttribute("listaJuegos", juegosRepository.findAll());
         return "juegos/lista";
     }
 
-    public String editarJuegos() {
+    @GetMapping("/editar")
+    public String editarJuegos(@RequestParam("id") int id,
+                               Model model) {
+        Optional<Juegos> opt = juegosRepository.findById(id);
+        if (opt.isPresent()){
+            Juegos juegoAEditar = opt.get();
+            model.addAttribute("juegoAEditar",juegoAEditar);
+            model.addAttribute("listaPlataformas", plataformasRepository.findAll());
+            return "juegos/editar";
+        }else{
+            return "redirect:/juegos";
+        }
 
     }
 
