f64f2e01db889e29c5c50fe89e8e881878fc1bd8
sarita atanacio <atanacio.sarita@pucp.pe>
2021-04-12 19:55:44 -0500

    Detalles y controlador

--- Lab1_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab1_gtics_20211/repository/DistribuidorasRepository.java
+++ Lab1_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab1_gtics_20211/repository/DistribuidorasRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DistribuidorasRepository extends JpaRepository<Distribuidoras,Integer> {
+public interface DistribuidorasRepository extends JpaRepository<Distribuidoras, Integer> {
 }
